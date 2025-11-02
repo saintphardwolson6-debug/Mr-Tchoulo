00ef3b87089915f9b823db94e0d4581530fd060e
saintphardwolson6-debug <saintphard.wolson6@gmail.com>
2025-11-02 10:16:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,9 @@
-require('dotenv').config();
-
-global.APIs = {
-    xteam: 'https://api.xteam.xyz',
-    dzx: 'https://api.dhamzxploit.my.id',
-    lol: 'https://api.lolhuman.xyz',
-    violetics: 'https://violetics.pw',
-    neoxr: 'https://api.neoxr.my.id',
-    zenzapis: 'https://zenzapis.xyz',
-    akuari: 'https://api.akuari.my.id',
-    akuari2: 'https://apimu.my.id',
-    nrtm: 'https://fg-nrtm.ddns.net',
-    bg: 'http://bochil.ddns.net',
-    fgmods: 'https://api-fgmods.ddns.net'
-};
-
-global.APIKeys = {
-    'https://api.xteam.xyz': 'd90a9e986e18778b',
-    'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-    'https://api.neoxr.my.id': 'yourkey',
-    'https://violetics.pw': 'beta',
-    'https://zenzapis.xyz': 'yourkey',
-    'https://api-fgmods.ddns.net': 'fg-dylux'
-};
-
-module.exports = {
-    WARN_COUNT: 3,
-    APIs: global.APIs,
-    APIKeys: global.APIKeys
+export default {
+  ownerNumber: "50936484174",
+  ownerName: "Mr Tchoulo",
+  botName: "Mr Tchoulo Bot",
+  prefix: ".",
+  channelLink: "https://whatsapp.com/channel/0029Vb6Xo1E35fLwGX6AWM43",
+  language: "fr",
+  aiEnabled: true,
 };
